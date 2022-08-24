667e60c1183528539acfb11cabcb4a9fc1af8a9a
chxxing <choing26@naver.com>
2022-08-24 17:48:43 +0900

    220824

---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const MainSlider = new Swiper('.MainSlider', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    })
+})
